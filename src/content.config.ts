874245edec439707aec4d66de0c319959baca3fe
Vinny Marquez <vinnymarquez@pm.me>
2025-07-08 22:45:58 +1000

    schema change

--- src/content.config.ts
+++ src/content.config.ts
@@ -18,10 +18,11 @@ const projects = defineCollection({
 const experiences = defineCollection({
   loader: glob({ pattern: '**/*.md', base: "./src/data/experience" }),
   schema: z.object({
-    role: z.array(z.string()),
+    roles: z.array(z.string()),
     company: z.string().optional(),
     location: z.string().optional(),
-    startDate: z.string().optional(),
+    icon: z.string().optional(),
+    startDate: z.date().optional(),
     endDate: z.string().optional(),
   }),
 });
